06ae85cc27d00fd668713f069964b2f6a7fa477b
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-03-27 15:58:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(name) {
-  name.push('Ralph')
+function destructivelyAppendKitten(myName) {
+  myName.push('Ralph')
+  return myName
 }
